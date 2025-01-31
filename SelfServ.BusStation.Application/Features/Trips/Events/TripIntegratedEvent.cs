e8c502a90dba6be2fcc39e21d0e28182f2757641
Нікіта Ковальчук <Nikita.Kovalchuk@systemgroup.com.ua>
2025-01-31 18:58:37 +0200

    add-trip-integration-event

---
+++ SelfServ.BusStation.Application/Features/Trips/Events/TripIntegratedEvent.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using SelfServ.BusStation.Application.DTOs;
+using SelfServ.BusStation.TripService.Application.Interfaces;
+
+namespace SelfServ.BusStation.TripService.Application.Features.Trips.Events
+{
+    public class TripIntegratedEvent : INotification
+    {
+        public List<TripDto> Trips { get; }
+        public TripIntegratedEvent(List<TripDto> trips)
+        {
+            Trips = trips;
+        }
+    }
+    public class TripIntegratedEventHandler : INotificationHandler<TripIntegratedEvent>
+    {
+        private readonly ITripIntegrationService _tripIntegrationService;
+
+        public TripIntegratedEventHandler(ITripIntegrationService tripIntegrationService)
+            => _tripIntegrationService = tripIntegrationService;
+
+        public async Task Handle(TripIntegratedEvent notification, CancellationToken cancellationToken)
+            => await _tripIntegrationService.IntegrateTripsAsync(notification.Trips);
+    }
+}
